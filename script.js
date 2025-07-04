63beaea05aadcbe0d91dc8a55101fb21cf420f98
Diego Moscoso <moscosovargasdiego@gmail.com>
2025-07-04 18:46:31 -0500

    Modificación archivo script.js

--- script.js
+++ script.js
@@ -1,32 +1,57 @@
-const webcam = document.getElementById("webcam");
-
-async function initCamera() {
-    try {
-        const stream = await navigator.mediaDevices.getUserMedia({ video: true });
-        webcam.srcObject = stream;
-    } catch (err) {
-        alert("No se pudo acceder a la cámara");
-        console.error("Error al acceder a la cámara:", err);
-    }
-}
-
-initCamera();
-
-
-//Simulacion de detecccion de frutas
+const URL = "./model/";
+let model, webcam, maxPredicions;
 
 const predictBtn = document.getElementById("predict-btn");
 const predictionBox = document.getElementById("prediction");
-const loading = document.getElementById("loading");
+const loadingText = document.getElementById("loadin");
+const historyList = document.getElementById("webcam");
 
-predictBtn.addEventListener("click", () => {
-  loading.classList.remove("hidden");
-  predictionBox.textContent = " Detectando...";
-});
+async function init() {
+    const modelURL = URL + "model.json";
+    const metadataURL = URL + "metadata.json";
 
+    model = await tmImage.load(modelURL, metadataURL);
+    maxPredicions = model.getTotalClasses();
 
-//Nota 1: Añadir emojis para mejor presentacion visual
+    webcam = new tmImage.Webcam(300, 300, true);
+    await webcam.setup();
+    await webcam.play();
 
+    videoElement.srcObject = webcam.webcam;
+    videoElement.addEventListener("loadeddata", () => {
+        webcam.update();
+    });
 
+    console.log("Modelo y cámara web listos");
+} 
 
-//Se debera de agregar un script para cambio de color de fondo segun con lo que se detecte
+async function predict() {
+    loadingText.classList.remove("hidden");
+    predictionBox.textContent = "Procesando...";
+
+    webcam.update();
+    const prediction = await model.predict(webcam.canvas);
+
+    let topClass = "";
+    let topProbability = 0;
+
+    prediction.forEach((p) => {
+        if (p.probability > topProbability) {
+            topProbability = p.probability;
+            topClass = p.className;
+        } 
+    });
+
+    const procentaje = (topProbability * 100).toFixed(1);
+    predictionBox.textContent = `Fruta detectada: ${topClass} (${porcentaje}%)`;
+
+    const li = document.createElement("li");
+    li.textContent = `${new Date().toLocaleTimeString()} → ${topClass} (${porcentaje}%)`;
+    historyList.prepend(li);
+
+    loadingText.classList.add("hidden");
+}
+
+predictBtn.addEventListener("click", predict);
+
+init();
